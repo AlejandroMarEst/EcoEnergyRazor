186905b38cd1f1e64d18db581662bb36891be476
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-26 10:33:09 +0100

    Finished designing functionality of Show Simulations page Closes #4

---
+++ Pages/SimCreator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcoEnergyRazor.Pages
+{
+    public class SimCreatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
